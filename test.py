472fbd5a7637b4bdee8191ea02dceeaa3f2899f4
aibybrum <bram_langmans@outlook.be>
2023-01-10 21:27:10 +0100

    design change

--- test.py
+++ test.py
@@ -27,11 +27,20 @@ app.layout = html.Div(className="center", children=[
                 ]),
             ]),
         ),
+        html.Div(className="swoops", children=[
+            #html.H2("Swoops"),
+            html.Div(className="window", children=[
+                dcc.RadioItems(
+                    ['09-01-2023', '10-01-2023', '11-01-2023', '12-01-2023'], '09-01-2023',
+                    inline=False, className="swoop", inputClassName="input_swoop", labelClassName="label_swoop"
+                ),
+            ]),
+        ]),
         html.Div(className="yaxis", children=[
             html.H2("Y-axis"),
             dcc.Checklist(
                 ['Elevation', 'Horizontal speed', 'Vertical speed', 'Dive angle'],
-                ['Elevation', 'Horizontal speed', 'Vertical speed'],
+                ['Elevation', 'Horizontal speed', 'Vertical speed'], 
                 className="y_check", inputClassName="input_y_check", labelClassName="label_y_check"
             )
         ]),
@@ -40,25 +49,33 @@ app.layout = html.Div(className="center", children=[
             dcc.RadioItems(['km/u', 'mph'], 'km/u', className="units_radio", inputClassName="input_units_radio", labelClassName="label_units_radio"),
             dcc.RadioItems(['m', 'ft'], 'm', inputClassName="input_units_radio", labelClassName="label_units_radio"),
         ]),
-        html.Div(className="swoops", children=[
-            html.H2("Swoops"),
-            html.Div(className="check", children=[
-                dcc.RadioItems(
-                    ['09-01-2023', '10-01-2023', '11-01-2023', '12-01-2023'], '09-01-2023',
-                    inline=False, className="item", inputClassName="input_item"
-                ),
-            ]),
-        ]),
         html.Div(className="save", children=[
             html.Button('Save graphs', id='save-val', n_clicks=0),
         ]),
     ]),
     html.Div(className="right", children=[
         html.Div(className="title", children=[
-            html.H1("Dashboard"),
+            html.H1("F@#k Ye4h!"),
             html.Div(className="line")
         ]),
-        
+        html.Div(className="metrics", children=[
+            html.Div(className="elevation", children=[
+                html.H3('493,58 ft', id='elevation'),
+                html.Label('Elevation'),
+            ]),
+            html.Div(className="horz_speed", children=[
+                html.H3('87,47 km/u', id='horz_speed'),
+                html.Label('Horizontal speed'),
+            ]),
+            html.Div(className="vert_speed", children=[
+                html.H3('112,66 km/u', id='vert_speed'),
+                html.Label('Vertical speed'),
+            ]),
+            html.Div(className="dive_angle", children=[
+                html.H3('40,10 °', id='dive_angle'),
+                html.Label('Dive angle'),
+            ]),
+        ]),
     ]),
 ])
 
