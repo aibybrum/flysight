4d168e85c27a9d16dda984326dc414a012aac28a
aibybrum <bram_langmans@outlook.be>
2023-03-03 11:16:05 +0100

    module change

---
+++ app/flysight/jump/jump.py
@@ -0,0 +1,23 @@
+from app.flysight.jump.landing.landing import Landing
+from app.flysight.jump.exit.exit import Exit
+import pandas as pd
+
+
+class Jump(Landing, Exit):
+    def __init__(self, name="empty", df=pd.DataFrame()):
+        self.name = name
+        if not df.empty:
+            self.df = df
+            super().__init__(self.df)
+
+    def get_df(self):
+        return self.df
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name}'
