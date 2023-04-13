c6c3936a8c0d2e51be2c1bc4157cc6667dbe1748
aibybrum <bram_langmans@outlook.be>
2023-04-13 17:51:58 +0200

    fixing detecting landing

--- app/flysight/jump/landing/landing.py
+++ app/flysight/jump/landing/landing.py
@@ -1,10 +1,12 @@
 import os
 import peakutils as pu
 import plotly.graph_objects as go
+import logging
 from dotenv import load_dotenv
-
-from flysight.jump import helpers
 from flysight.jump.exit.exit import Exit
+import flysight.jump.helpers as helpers
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 token = os.getenv("TOKEN")
@@ -14,39 +16,68 @@ class Landing:
     def __init__(self, df):
         self.df = df
         self.exit_df = Exit(df).get_exit_df()
-        self.landing_df = self.exit_df.iloc[self.get_landing():].reset_index(drop=True)
+        self.landing_df = self.exit_df.iloc[self.get_landing_index():].reset_index(drop=True)
+    
+    def get_landing_df(self):
+        return self.landing_df
+        
+    # Get landing
+    def get_landing_elevation(self):
+        jmp_df = self.exit_df[self.exit_df.elevation <= 1200].reset_index()
+        peaks = pu.indexes(jmp_df.elevation, thres=0.5, min_dist=0.1)
+        lows = pu.indexes(-jmp_df.elevation, thres=0.1, min_dist=0.1)
+        return {'start_landing': jmp_df, 'peaks': peaks, 'lows': lows}
 
+    def get_landing_vert_speed(self):
+        jmp_df = self.get_landing_elevation()['start_landing']
+        peaks = pu.indexes(jmp_df.vert_speed_mph, thres=0.5, min_dist=0.1)
+        lows = pu.indexes(-jmp_df.vert_speed_mph, thres=0.75, min_dist=5)
+        
+        lows_keys = [low for low in lows if low <= peaks[-1]]
+        lows_values = [jmp_df.vert_speed_mph[low] for low in lows_keys]
+        
+        # landing_point is where the vertspeed is the lowest and start to increase
+        return {'landing_point': lows_keys[lows_values.index(min(lows_values))], 'peaks': peaks, 'lows': lows}
+    
     def get_landing(self):
-        elevation_lows = pu.indexes(-self.exit_df.elevation, thres=0.5, min_dist=1)
-        # get last low point above 250 feet
-        return [elevation_lows[i] for i in range(0, len(elevation_lows))
-                if self.exit_df.elevation[elevation_lows[i]] > 250][-1]
-
-    def get_top_of_turn(self):
-        return pu.indexes(self.landing_df.elevation, thres=0.01, min_dist=1)[0]
-
+        # change landing point if there is a dip in elevation
+        elevation_low = self.get_landing_elevation()['lows'][0]
+        vert_speed_low = self.get_landing_vert_speed()['landing_point']
+        if elevation_low <= vert_speed_low:
+            return elevation_low
+        return vert_speed_low
+    
+    def get_landing_index(self):
+        try:
+            jmp_df = self.get_landing_elevation()['start_landing']
+            return self.exit_df.index[self.exit_df['time'] == jmp_df.time[self.get_landing()]][0]
+        except:
+            logging.info(f'No high performance landing detected')
+        
+    # Get features of landing
     def get_stop(self):
         offset = 1.5
         horz_speed_peaks = pu.indexes(self.landing_df.horz_speed_mph, thres=0.1, min_dist=1)
         horz_speed_lows = pu.indexes(-self.landing_df.horz_speed_mph, thres=0.5, min_dist=1)
-        return [l for l in horz_speed_lows if l > horz_speed_peaks[-1] and self.landing_df.horz_speed_mph[l] < offset][
-            0]
+        return [l for l in horz_speed_lows if l > horz_speed_peaks[-1] and self.landing_df.horz_speed_mph[l] < offset][0]
+    
+    def get_top_of_turn(self):
+        peaks = pu.indexes(self.landing_df.elevation, thres=0.01, min_dist=1)
+        if len(peaks) == 0 or peaks[0] >= self.get_stop():
+            return None
+        return peaks[0]
 
     def get_max_horz_speed(self):
         return self.landing_df.idxmax().horz_speed_mph
-
+    
     def set_startpoint(self, startpoint):
         if startpoint == 'Start':
             self.landing_df = helpers.set_start_point(self.landing_df, 0)
         elif startpoint == 'Top of turn':
-            self.landing_df = helpers.set_start_point(self.landing_df, self.get_top_of_turn())
+            if self.get_top_of_turn() is not None:
+                self.landing_df = helpers.set_start_point(self.landing_df, self.get_top_of_turn())
 
-    def get_landing_df(self):
-        return self.landing_df
-
-    def save_landing(self, name):
-        self.landing_df.to_csv(f'././data/landing/{name}.csv', index=False)
-
+    # Visualisations
     def plt_overview(self, selected, speed_metric, distance_metric):
         if len(selected) != 0:
             x_axis = helpers.get_x_axis_settings(self.landing_df, distance_metric=distance_metric)
@@ -93,12 +124,12 @@ class Landing:
 
         fig = go.Figure()
         fig.add_trace(go.Scatter(
-            x=x_axis['Distance']['col'],
-            y=y_axis['Distance']['col'],
+            x=x_axis['Distance']['col'].iloc[:self.get_stop()+1],
+            y=y_axis['Distance']['col'].iloc[:self.get_stop()+1],
             line=dict(color=y_axis['Distance']['color'], width=1.2),
             hovertemplate=y_axis['Distance']['hovertemplate'],
             showlegend=False,
-
+            
         ))
         fig.update_layout(hovermode='x unified',
                           legend=dict(yanchor="top", y=1, xanchor="right", x=1))
@@ -110,25 +141,28 @@ class Landing:
         x_axis = helpers.get_x_axis_settings(self.landing_df, distance_metric=distance_metric)
         y_axis = helpers.get_y_axis_settings(self.landing_df)
 
+        top = self.get_top_of_turn()
+
         fig = go.Figure()
         fig.add_trace(go.Scatter(
-            x=x_axis['Horizontal Distance']['col'].iloc[:self.get_stop() + 1],
-            y=y_axis['Elevation']['col'].iloc[:self.get_stop() + 1],
+            x=x_axis['Horizontal Distance']['col'].iloc[:self.get_stop()+1],
+            y=y_axis['Elevation']['col'].iloc[:self.get_stop()+1],
             name='Elevation',
             line=dict(color=y_axis['Elevation']['color'], width=1.2),
             hovertemplate=y_axis['Elevation']['hovertemplate'],
             showlegend=False
         ))
-        fig.add_trace(go.Scatter(
-            x=[x_axis['Horizontal Distance']['col'][self.get_top_of_turn()]],
-            y=[y_axis['Elevation']['col'][self.get_top_of_turn()]],
-            name='Top of turn',
-            text=['Top of turn'],
-            textposition='top right',
-            hovertemplate='Top of turn: %{y:.2f}' + y_axis['Elevation']['metric'] + ' <extra></extra>',
-            mode='markers+text',
-            showlegend=False
-        ))
+        if top is not None:
+            fig.add_trace(go.Scatter(
+                x=[x_axis['Horizontal Distance']['col'][top]],
+                y=[y_axis['Elevation']['col'][top]],
+                name='Top of turn',
+                text=['Top of turn'],
+                textposition='top right',
+                hovertemplate='Top of turn: %{y:.2f}' + y_axis['Elevation']['metric'] + ' <extra></extra>',
+                mode='markers+text',
+                showlegend=False
+            ))
         fig.add_trace(go.Scatter(
             x=[x_axis['Horizontal Distance']['col'][self.get_stop()]],
             y=[y_axis['Elevation']['col'][self.get_stop()]],
@@ -187,6 +221,8 @@ class Landing:
         df = self.landing_df.iloc[:self.get_stop() + 1].reset_index(drop=True)
         y_axis = helpers.get_y_axis_settings(df)
 
+        top = self.get_top_of_turn()
+
         fig = go.Figure()
 
         fig.add_trace(go.Scattermapbox(
@@ -196,14 +232,14 @@ class Landing:
             line=dict(color=y_axis['Elevation']['color']),
             showlegend=False,
         ))
-
-        fig.add_trace(go.Scattermapbox(
-            lat=[df.lat[self.get_top_of_turn()]],
-            lon=[df.lon[self.get_top_of_turn()]],
-            name='Top of turn',
-            textposition='top right',
-            mode='markers',
-        ))
+        if top is not None:
+            fig.add_trace(go.Scattermapbox(
+                lat=[df.lat[self.get_top_of_turn()]],
+                lon=[df.lon[self.get_top_of_turn()]],
+                name='Top of turn',
+                textposition='top right',
+                mode='markers',
+            ))
 
         fig.update_layout(
             margin=dict(l=0, r=0, t=0, b=0),
@@ -220,38 +256,4 @@ class Landing:
             )
         )
         return fig
-
-    def debug(self, yaxis, speed_metric='km/u', distance_metric='m'):
-        x_axis = helpers.get_x_axis_settings(self.landing_df, distance_metric=distance_metric)
-        y_axis = helpers.get_y_axis_settings(self.landing_df, speed_metric=speed_metric)
-
-        vert_speed_peaks = pu.indexes(y_axis[yaxis]['col'], thres=0.5, min_dist=1)
-        vert_speed_lows = pu.indexes(-y_axis[yaxis]['col'], thres=0.5, min_dist=1)
-
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-            x=x_axis['Distance']['col'],
-            y=y_axis[yaxis]['col'],
-            name=yaxis,
-            line=dict(color=y_axis[yaxis]['color'], width=1.2),
-            hovertemplate=y_axis[yaxis]['hovertemplate'],
-            showlegend=False
-        ))
-        fig.add_trace(go.Scatter(
-            x=x_axis['Distance']['col'][vert_speed_peaks],
-            y=y_axis[yaxis]['col'][vert_speed_peaks],
-            mode='markers',
-            marker=dict(color='Green', size=8),
-            showlegend=False
-        ))
-        fig.add_trace(go.Scatter(
-            x=x_axis['Distance']['col'][vert_speed_lows],
-            y=y_axis[yaxis]['col'][vert_speed_lows],
-            mode='markers',
-            marker=dict(color='Red', size=8),
-            showlegend=False
-        ))
-        fig.update_layout(hovermode="x unified", legend=dict(yanchor="top", y=1, xanchor="right", x=1))
-        fig.update_xaxes(title=f"Horizontal distance ({x_axis['Distance']['metric']})")
-        fig.update_yaxes(title=f"{yaxis} ({y_axis[yaxis]['metric']})")
-        return fig
+    
