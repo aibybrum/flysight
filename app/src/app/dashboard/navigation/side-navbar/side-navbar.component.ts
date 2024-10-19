f8aa1f4690e513b967007be405df3c9d51d2ff5f
Bram.Langmans <bram.langmans@ing.com>
2024-10-19 20:31:25 +0200

    changes to data and frontend

--- app/src/app/dashboard/navigation/side-navbar/side-navbar.component.ts
+++ app/src/app/dashboard/navigation/side-navbar/side-navbar.component.ts
@@ -1,41 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
   styleUrl: './side-navbar.component.scss'
 })
-export class SideNavbarComponent implements OnInit {
-  constructor() {}
+export class SideNavbarComponent {
 
-  ngOnInit() {
-    const body = document.querySelector("body");
-    if (body) {
-      document.querySelectorAll(".sidebar .nav-item").forEach(function(el) {
-        el.addEventListener("mouseover", function() {
-          if (body.classList.contains("sidebar-icon-only")) {
-            el.classList.add("hover-open");
-          }
-        });
-        el.addEventListener("mouseout", function() {
-          if (body.classList.contains("sidebar-icon-only")) {
-            el.classList.remove("hover-open");
-          }
-        });
-      });
-    }
-  }
-
-  public parentId = "";
-  clickedMenu(event: MouseEvent) {
-    var target = event.currentTarget as HTMLElement;
-    let parentId = target.id;
-    if (parentId == this.parentId) {
-      console.log('same');
-      this.parentId = "";
-    } else {
-      console.log('not same');
-      this.parentId = target.id;
-    }
-  }
 }
--- app/src/app/dashboard/navigation/top-navbar/top-navbar.component.ts
+++ app/src/app/dashboard/navigation/top-navbar/top-navbar.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit} from '@angular/core';
-import { NgbDropdownConfig } from "@ng-bootstrap/ng-bootstrap";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-navbar',
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.scss',
-  providers: [NgbDropdownConfig]
 })
-export class TopNavbarComponent implements OnInit {
-  public iconOnlyToggled = false;
-  public sidebarToggled = false;
-
-  toggleRightSidebar() {
-    const sidebarOffcanvas = document.querySelector('.sidebar-offcanvas');
-    if (sidebarOffcanvas) {
-      sidebarOffcanvas.classList.toggle('active');
-    } else {
-      console.warn("No element with class 'sidebar-offcanvas' found.");
-    }
-  }
-
-  toggleIconOnlySidebar() {
-    this.iconOnlyToggled =!this.iconOnlyToggled;
-    (document.querySelector("body")?? document.body).classList.toggle("sidebar-icon-only", this.iconOnlyToggled);
-  }
-
-  constructor(config: NgbDropdownConfig) {
-    config.placement = "bottom-right";
-  }
-  ngOnInit() {}
+export class TopNavbarComponent {
+  
 }
